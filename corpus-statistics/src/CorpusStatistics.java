079328d1d7d9f0daa99b3377a0b68f4b0f5b49b8
mcmyers12 <mirandacmyers12@gmail.com>
2017-08-31 20:36:36 -0400

    Some statistics calculated

--- corpus-statistics/src/CorpusStatistics.java
+++ corpus-statistics/src/CorpusStatistics.java
@@ -1,8 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class CorpusStatistics {
 
@@ -12,14 +21,13 @@ public class CorpusStatistics {
 	private int numDocuments = 0; // Number of paragraphs processed
 	private int vocabularySize = 0; // Number of unique words observed
 	private int collectionSize = 0; // Total number of words encountered
-	private int numWordsInOneDocument = 0; // Number of words that occur in
-											// exactly one document
-	private Map<String, Term> lexicon = new HashMap<>();
+	private int numWordsInOneDocument = 0; // Number of words that occur in exactly one document
+	
+	private HashMap<String, Term> lexicon = new LinkedHashMap<>();
 
 	private class Term {
-		private int collectionFrequency = 0; // Number of times term is seen
-		private int documentFrequency = 0; // Number of documents which the word
-											// occurs in
+		private int collectionFrequency = 1; // Number of times term is seen
+		private int documentFrequency = 1; // Number of documents which the word occurs in
 	}
 
 	private void readFile(String fileName) {
@@ -31,13 +39,44 @@ public class CorpusStatistics {
 
 			String currentLine;
 
-			while ((currentLine = bufferedReader.readLine()) != null) {
-				if (currentLine.startsWith("<P ID=")) {
+			int count = 0;
+			while ((currentLine = bufferedReader.readLine()) != null && count < 3) {
+				if (currentLine.startsWith("<P ID=")) { // The start of a new document (paragraph)
+					count ++;
+					numDocuments++;
 					currentLine = bufferedReader.readLine();
-					String[] tokens = tokenize(currentLine);
+					Set<String> tokensInDocument = new HashSet<>();
+					
+					while (currentLine != null && !currentLine.startsWith("</P>")) {
+						String[] tokens = tokenize(currentLine);
+						
+						for (String token : tokens) {
+							collectionSize++;
+							
+							tokensInDocument.add(token);
+							if (lexicon.containsKey(token)) {
+								lexicon.get(token).collectionFrequency++; // Increment number of times term is seen
+							}
+							else {
+								lexicon.put(token, new Term());
+							}
+						}
+						
+						currentLine = bufferedReader.readLine();
+					}
+					
+					for (String token : tokensInDocument) {
+						lexicon.get(token).documentFrequency++; // Increment number of documents each token occurs in
+					}
+					
 				}
-				System.out.println(currentLine);
 			}
+			
+			vocabularySize = lexicon.keySet().size();
+			
+			sortLexicon();
+			calculateNumWordsInOneDocument();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -52,6 +91,55 @@ public class CorpusStatistics {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	//Assumes sorted lexicon
+	private void calculateNumWordsInOneDocument() {
+		for (Term term : lexicon.values()) {
+			if (term.documentFrequency == 1) {
+				numWordsInOneDocument++;
+			}
+			else {
+				return;
+			}
+		}
+	}
+	
+	private void sortLexicon() { 
+	   List<Entry<String, Term>> list = new LinkedList<Entry<String, Term>>(lexicon.entrySet());
+	   // Defined Custom Comparator here
+	   Collections.sort(list, new Comparator<Entry<String, Term>>() {
+	        public int compare(Entry<String, Term> o1, Entry<String, Term> o2) {
+	           return ((Comparable<Integer>) ((Map.Entry<String, Term>) (o1)).getValue().collectionFrequency)
+	              .compareTo(((Map.Entry<String, Term>) (o2)).getValue().collectionFrequency);
+	        }
+	   });
+	
+	   // Here I am copying the sorted list in HashMap
+	   // using LinkedHashMap to preserve the insertion order
+	   HashMap<String, Term> sortedHashMap = new LinkedHashMap<>();
+	   for (Iterator<Entry<String, Term>> it = list.iterator(); it.hasNext();) {
+	          Map.Entry<String, Term> entry = (Map.Entry<String, Term>) it.next();
+	          sortedHashMap.put(entry.getKey(), entry.getValue());
+	   } 
+	   lexicon = sortedHashMap;
+	  }
+	
+	private void printResults() {
+		for (Entry<String, Term> entry : lexicon.entrySet()) {
+			System.out.println(entry.getKey());
+			System.out.println("\tcollectionFrequency: " + entry.getValue().collectionFrequency);
+			System.out.println("\tdocumentFrequency: " + entry.getValue().documentFrequency);
+		}
+		
+		System.out.println("\n\n\nNumber of documents (paragraphs) processed");
+		System.out.println("\t" + numDocuments);
+		System.out.println("\nVocabulary size (number of unique words observed)");
+		System.out.println("\t" + vocabularySize);
+		System.out.println("\nCollection size (total number of words encountered)");
+		System.out.println("\t" + collectionSize);
+		System.out.println("\nNumber of words uccuring in exactly one document");
+		System.out.println("\t" + numWordsInOneDocument);
 	}
 
 	/**
@@ -79,11 +167,8 @@ public class CorpusStatistics {
 	// Test program on two text files
 	public static void main(String[] args) {
 		CorpusStatistics corpusStatistics = new CorpusStatistics();
-		// corpusStatistics.readFile(CorpusStatistics.BIBLE_FILE);
-		for (String token : corpusStatistics.tokenize("''H-i Mir'anda... HEY")) {
-			System.out.println(token);
-		}
-
+		corpusStatistics.readFile(CorpusStatistics.BIBLE_FILE);
+		corpusStatistics.printResults();
 	}
 
 }
