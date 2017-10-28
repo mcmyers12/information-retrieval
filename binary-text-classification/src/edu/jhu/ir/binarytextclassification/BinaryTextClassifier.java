db602d196e89cc882b0ba305942daa0e8958e5be
mcmyers12 <mirandacmyers12@gmail.com>
2017-10-28 13:48:33 -0400

    Binary text classifier setup

---
+++ binary-text-classification/src/edu/jhu/ir/binarytextclassification/BinaryTextClassifier.java
@@ -0,0 +1,13 @@
+package edu.jhu.ir.binarytextclassification;
+
+/**
+ * This class builds a binary classifier using training data and then uses the trained model to
+ * 		make predictions on the test data.
+ *
+ * @author Miranda Myers
+ *
+ */
+
+public class BinaryTextClassifier {
+
+}
